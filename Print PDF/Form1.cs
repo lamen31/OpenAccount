4d293e8648e5d550d20f4c4139317a59f16ce785
mpekik <64069485+mpekik@users.noreply.github.com>
2021-01-08 12:28:58 +0700

    printpdf

--- Print PDF/Form1.cs
+++ Print PDF/Form1.cs
@@ -1,5 +1,4 @@
-﻿using Patagames.Pdf.Net;
-using Patagames.Pdf.Net.Controls.WinForms;
+﻿using Spire.Pdf;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -24,12 +23,35 @@ namespace Print_PDF
         private void Form1_Load(object sender, EventArgs e)
         {
             this.Hide();
-            var doc = PdfDocument.Load(@"c:\Test\testpdf.pdf");
-            var printDoc = new PdfPrintDocument(doc);
-            PrintController printController = new StandardPrintController();
-            printDoc.PrintController = printController;
-            printDoc.PrinterSettings.PrinterName = "Brother HL-L5100DN Series";
-            printDoc.Print(); // Print PDF document
+            //var doc = PdfDocument.Load(@"c:\Test\testpdf.pdf");
+            //var printDoc = new PdfPrintDocument(doc);
+            PdfDocument doc = new PdfDocument();
+            doc.LoadFromFile(@"c:\Test\testpdf.pdf");
+            //PrintController printController = new StandardPrintController();
+            //printDoc.PrintController = printController;
+            //printDoc.PrinterSettings.PrinterName = "Brother HL-L2360D series";
+            //printDoc.Print(); // Print PDF document
+
+            //PrintDialog dialogPrint = new PrintDialog();
+            //dialogPrint.AllowPrintToFile = true;
+            //dialogPrint.AllowSomePages = true;
+            //dialogPrint.PrinterSettings.MinimumPage = 1;
+            //dialogPrint.PrinterSettings.MaximumPage = doc.Pages.Count;
+            //dialogPrint.PrinterSettings.FromPage = 1;
+            //dialogPrint.PrinterSettings.ToPage = doc.Pages.Count;
+            //if (dialogPrint.ShowDialog() == DialogResult.OK)
+            //{
+            //    doc.PrintFromPage = dialogPrint.PrinterSettings.FromPage;
+            //    doc.PrintToPage = dialogPrint.PrinterSettings.ToPage;
+            //    doc.PrinterName = "Brother HL-L2360D series"    ;
+            //    PrintDocument printDoc = doc.PrintDocument;
+            //    printDoc.Print();
+            //}
+            doc.PrintSettings.PrintController = new StandardPrintController();
+            doc.PrinterName = "Brother HL-L5100DN Series";
+            PrintDocument printdoc = doc.PrintDocument;
+            printdoc.PrintController = new StandardPrintController();
+            printdoc.Print();
             this.Close();
         }
     }
