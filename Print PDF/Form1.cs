4abb4aed72c582697d6626d6144d8f6a1275b710
andreas lamen <andreas.lamen@gmail.com>
2021-01-08 12:26:42 +0700

    Pembetulan print pdf

--- Print PDF/Form1.cs
+++ Print PDF/Form1.cs
@@ -1,5 +1,8 @@
-﻿using Patagames.Pdf.Net;
-using Patagames.Pdf.Net.Controls.WinForms;
+﻿//using Patagames.Pdf.Net;
+//using Patagames.Pdf.Net.Controls.WinForms;
+//using Spire;
+//using Spire.Pdf;
+using Spire.Pdf;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -24,12 +27,19 @@ namespace Print_PDF
         private void Form1_Load(object sender, EventArgs e)
         {
             this.Hide();
-            var doc = PdfDocument.Load(@"c:\Test\testpdf.pdf");
-            var printDoc = new PdfPrintDocument(doc);
+            PdfDocument doc = new PdfDocument();
+            doc.LoadFromFile(@"c:\Test\testpdf.pdf");
+            doc.PrinterName = "Brother HL-L5100DN Series";
+
+            PrintDocument printdoc = new PrintDocument();
+
+            //printdoc.DocumentName = @"c:\Test\testpdf.pdf";
+            //var doc = printdoc.Load(@"c:\Test\testpdf.pdf");
+            //var printDoc = new PdfPrintDocument(doc);
             PrintController printController = new StandardPrintController();
-            printDoc.PrintController = printController;
-            printDoc.PrinterSettings.PrinterName = "Brother HL-L5100DN Series";
-            printDoc.Print(); // Print PDF document
+            printdoc.PrintController = printController;
+            //printdoc.PrinterSettings.PrinterName = "Brother HL-L5100DN Series";
+            printdoc.Print(); // Print PDF document
             this.Close();
         }
     }
